34ebc967aa7603028daa9c10d20beb6e59a20ae1
milanovicn <63123416+milanovicn@users.noreply.github.com>
2020-12-03 21:59:54 +0100

    Ubicu se

---
+++ ISA-Backend/src/test/java/com/example/ISABackend/IsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISABackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
